15ce12be9aa69c35686736dc04e4366ff60ba9bf
FelixTran <felixtran2000@gmail.com>
2024-06-29 19:44:14 +0700

    init

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Currency Exchange Portal",
+  description: "Created by create-next-app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+    
+              {children}
+              {/* <Analytics /> */}
+              <Toaster />
+           
+            </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main className="relative flex min-h-screen flex-col max-w-[100vw] max-h-[100vh] overflow-hidden  items-center justify-center gap-4 p-4">
+      
+    </main >
+  );
+}
